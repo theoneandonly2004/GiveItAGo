af1cfdb7c373f9c89eefbf0442c0f5087012e5ae
theoneandonly <dannyboy204@outlook.com>
2016-10-09 12:22:38 +0100

    started work on basic training exercises

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    float delayTime = 5.0f;
+    public GameObject balloonObject;
+    float maxXDiff = 2.5f , minXDiff = -0.5f;
+    float maxZDiff = 2.5f, minZDiff = -0.5f;
+    int level = 1;
+    GameObject[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoints = GameObject.FindGameObjectsWithTag("Spawner");
+        Invoke("Spawn", delayTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn()
+    {      
+       GameObject spawnedObject = Instantiate(balloonObject);
+        Vector3 spawnPosition = GetSpawnPoint();
+        spawnedObject.transform.position = spawnPosition;
+        Invoke("Spawn", delayTime);
+    }
+
+    Vector3 GetSpawnPoint()
+    {
+        int xDiff = (int)Random.Range(minXDiff, maxXDiff);
+        int zDiff = (int)Random.Range(minZDiff, maxZDiff);
+
+        int spawnPoint = (int)Random.Range(0, spawnPoints.Length - 1);
+
+        float posX = spawnPoints[spawnPoint].transform.position.x + xDiff;
+        float posY = spawnPoints[spawnPoint].transform.position.y;
+        float posZ = spawnPoints[spawnPoint].transform.position.z + zDiff;
+
+        
+
+        return new Vector3(posX, posY, posZ);
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("i was hit by " + collision.name);
+    }
+}
